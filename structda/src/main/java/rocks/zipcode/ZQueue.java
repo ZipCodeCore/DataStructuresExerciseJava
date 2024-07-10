d85a0af310f19e44707a784a8729379d915ff5af
Kristofer <kris@youngers.org>
2024-07-10 08:57:27 -0400

    init

---
+++ structda/src/main/java/rocks/zipcode/ZQueue.java
@@ -0,0 +1,13 @@
+package rocks.zipcode;
+
+public interface ZQueue<T> {
+    boolean isEmpty();
+    
+    int size();
+    
+    void enqueue(T element);
+    
+    T dequeue();
+    
+    T peek();
+}
